58d4dadaecc717eb7d74a9b59437d9e1160389c1
bruno <bybrunobarros@gmail.com>
2020-09-09 15:24:13 +0200

    feat(bowling): calculate score with spare bonus

--- bowling/index.test.ts
+++ bowling/index.test.ts
@@ -16,3 +16,20 @@ test("when 10 frames the game should return a score", (t) => {
   ]);
   t.is(score, 20);
 });
+
+test("when 10 spares the game should return a score with spare bonuses", (t) => {
+  const score = play([
+    [5, 5],
+    [5, 5],
+    [5, 5],
+    [5, 5],
+    [5, 5],
+    [5, 5],
+    [5, 5],
+    [5, 5],
+    [5, 5],
+    [5, 5],
+    [5],
+  ]);
+  t.is(score, 150);
+});
--- bowling/index.ts
+++ bowling/index.ts
@@ -1,7 +1,7 @@
-type None = "None";
-type Spare = "Spare";
-type Strike = "Strike";
-type Bonus = None | Spare | Strike;
+type NONE = "none";
+type SPARE = "spare";
+type STRIKE = "strike";
+type Bonus = NONE | SPARE | STRIKE;
 
 type Frame = [number, number?];
 
@@ -20,12 +20,49 @@ type Game = [
   Frame?,
 ];
 
-export function play(frames: Game): number {
-  return frames.reduce((acc: number, frame: Frame | undefined): number => {
-    if (frame === undefined) return acc;
+const NONE: NONE = "none";
+const SPARE: SPARE = "spare";
 
-    const [firstTry, secondTry = 0] = frame;
-    acc = acc + firstTry + secondTry;
-    return acc;
-  }, 0);
+function isSpare(firstTry: number, secondTry: number): boolean {
+  return firstTry < 10 && firstTry + secondTry === 10;
+}
+
+function getBonusType(firstTry: number, secondTry: number): Bonus {
+  if (isSpare(firstTry, secondTry)) {
+    return SPARE;
+  }
+
+  return NONE;
+}
+
+export function play(frames: Game): number {
+  let bonus: Bonus = NONE;
+
+  return frames.reduce(
+    (acc: number, frame: Frame | undefined, index: number): number => {
+      if (index >= 10 || frame === undefined) return acc;
+
+      const [firstTry, secondTry = 0] = frame;
+      bonus = getBonusType(firstTry, secondTry);
+
+      if (bonus === NONE) {
+        acc = acc + firstTry + secondTry;
+      }
+      if (bonus === SPARE) {
+        const bonusScore = frames
+          .slice(index + 1)
+          .flat()
+          .slice(0, 1)
+          .reduce((acc, pins) => {
+            if (acc !== undefined && pins !== undefined) {
+              acc = acc + pins;
+            }
+            return acc;
+          }, 0);
+        acc = acc + firstTry + secondTry + (bonusScore || 0);
+      }
+      return acc;
+    },
+    0,
+  );
 }
